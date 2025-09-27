a2c34ae13406ccf3d5b48e5db7440048fb92abe6
micbab <141713243+micbab@users.noreply.github.com>
2025-09-27 20:25:16 +0200

    Add Main class with initial setup

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(
+            "Luke",
+            "Skywalker",
+            19,
+            1250.50);
+    }
+}
