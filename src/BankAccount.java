d3b3c589875a4bcd1fad6fde6f5132eb67d28796
micbab <141713243+micbab@users.noreply.github.com>
2025-09-27 20:39:33 +0200

    Implement Getters and Setters for BankAccount fields

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -19,4 +19,36 @@ public class BankAccount {
         System.out.println("Age: " + this.age);
         System.out.printf("Balance (USD): $%.2f%n", this.balance);
     }
+
+        public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
 }
