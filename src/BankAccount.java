f4a950a0dda7c76578e1f16f1a3c7ff97a54bad7
micbab <141713243+micbab@users.noreply.github.com>
2025-09-27 20:18:41 +0200

    Add constructor to BankAccount class

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -4,4 +4,11 @@ public class BankAccount {
     private String lastName;
     private int age;
     private double balance;
+
+    public BankAccount(String firstName, String lastName, int age, double balance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.balance = balance;
+    }
 }
